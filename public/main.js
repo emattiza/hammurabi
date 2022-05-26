6789e141053f6748ff6e093441e0c2ea25a97618
Evan Mattiza <emattiza@gmail.com>
2022-05-26 17:48:47 +0000

    added some basic functionality and terminal

--- public/main.js
+++ public/main.js
@@ -1,5 +1,18 @@
 import "./style.css";
 import { Elm } from "../src/Main.elm";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
 
 const root = document.querySelector("#app div");
 const app = Elm.Main.init({ node: root });
+window.addEventListener("load", () => {
+  const term = new Terminal({ cursorBlink: true });
+  const fitAddon = new FitAddon();
+  term.loadAddon(fitAddon);
+  term.open(document.getElementById("terminal"));
+  fitAddon.fit();
+  term.onData;
+  term.writeln("\x1b[1;32mWelcome to Hammurabi!");
+  term.write("> ");
+});
